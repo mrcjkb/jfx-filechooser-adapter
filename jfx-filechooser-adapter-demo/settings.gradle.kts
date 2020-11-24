a248d3a6f070c6bda3d3bbd2d7dfeedefec878a2
Marc Jakobi <marc.jakobi@velasolaris.com>
2020-11-24 21:21:26 +0100

    Add demo app.

---
+++ jfx-filechooser-adapter-demo/settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "jfx-filechooser-adapter-demo"
+
+include(":jfx-filechooser-adapter", ":jfx-filechooser-adapter-api", ":jfx-filechooser-adapter-impl")
+project(":jfx-filechooser-adapter").projectDir = File(settingsDir, "../jfx-filechooser-adapter-parent")
+project(":jfx-filechooser-adapter-api").projectDir = File(settingsDir, "../jfx-filechooser-adapter-api")
+project(":jfx-filechooser-adapter-impl").projectDir = File(settingsDir, "../jfx-filechooser-adapter-impl")
