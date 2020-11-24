580ebf71fa863590f650e5bd2704b050bb89b2cd
Marc Jakobi <marc.jakobi@velasolaris.com>
2020-11-24 21:22:08 +0100

    Add JfxFileChooserAdapterModuleContext.

---
+++ jfx-filechooser-adapter-impl/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.github.mrcjkb.jfxfilechooseradapter.impl {
+    exports com.github.mrcjkb.jfxfilechooseradapter.impl;
+    requires com.github.mrcjkb.jfxfilechooseradapter.api;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires kotlin.stdlib;
+    requires java.prefs;
+}
---
+++ jfx-filechooser-adapter-parent/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.github.mrcjkb.jfxfilechooseradapter {
+    exports com.github.mrcjkb.jfxfilechooseradapter;
+    requires transitive com.github.mrcjkb.jfxfilechooseradapter.api;
+    requires com.github.mrcjkb.jfxfilechooseradapter.impl;
+}
